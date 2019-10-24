0b9f799a73eb98692c685e18a90b87cd49747ef8
Jacek <mynaj@student.mini.pw.edu.pl>
2019-10-24 18:07:14 +0200

    3 tests added.

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,4 +1,15 @@
 library(testthat)
 library(L4)
 
-test_check("L4")
+test_that("output values correctness integer", {
+  expect_equal(avgVecList(list(c(20,10), c(4,10,7))), list(15,7))
+})
+
+test_that("output values correctness double", {
+  expect_equal(avgVecList(list(c(15,10), c(1,3,3,3))), list(12.5,2.5))
+})
+
+test_that("output length correctness", {
+  expect_equal(length(avgVecList(list(c(15,10), c(1,3,3,3)))), 2)
+})
+
